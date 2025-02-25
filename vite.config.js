58d436266cdfab68ea575f1ea9857bf5b925bdc5
luizlacerdam <luizlacerdam.dev@gmail.com>
2025-02-25 12:03:08 -0500

    updating vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Ensures correct asset resolution
+  build: {
+    outDir: 'dist', // This is the default for Vite, but ensure it's correct
+  },
+  server: {
+    host: true, // Allows network access
+  }
+});
